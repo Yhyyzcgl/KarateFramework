d8409aa4b90449372281f9d3ebf58ad82190bd50
DevOps <k95599k@gmail.com>
2024-11-14 13:33:34 +0300

    14.11.24

--- src/test/java/KarateTestRunner.java
+++ src/test/java/KarateTestRunner.java
@@ -4,6 +4,8 @@ public class KarateTestRunner {
 
     @Karate.Test
     Karate testApi() {
-        return Karate.run("classpath:apiTest.feature","classpath:sample_post_test.feature","classpath:sample_test.feature").relativeTo(getClass());
+        return Karate.run("classpath:apiTest.feature","classpath:sample_post_test.feature","classpath:sample_test.feature","classpath:get-example.feature").relativeTo(getClass());
     }
+
 }
+
